6830f1977146b73194d127b29932e5e3919c5e97
yasersomani-glitch <yasersomani@gmail.com>
2026-08-28 06:40:25 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php declare(strict_types=1); file_put_contents( __DIR__ . '/telegram_debug.log', date('Y-m-d H:i:s') . "\n" . file_get_contents('php://input') . "\n\n", FILE_APPEND ); http_response_code(200); echo 'OK'; exit;
+```php
+<?php
+
+declare(strict_types=1);
+
+file_put_contents(
+    __DIR__ . '/telegram_debug.log',
+    date('Y-m-d H:i:s') . "\n" .
+    file_get_contents('php://input') . "\n\n",
+    FILE_APPEND
+);
+
+http_response_code(200);
+
+echo 'OK';
+
+exit;
+```
