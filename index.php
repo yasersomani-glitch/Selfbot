5a4ade211337454e513c7466fb7d2bd76bfc0cce
yasersomani-glitch <yasersomani@gmail.com>
2026-08-28 05:26:46 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,10 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 
+echo 'PHP WORKS';
+exit;
+
+
 
 /*
 |--------------------------------------------------------------------------
